6eda4071c00db1b57401f79c37928e2e9d45d279
Leticia Madruga <leticia.madruga@escola.pr.gov.br>
2025-10-21 09:24:11 -0300

    21/10

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const botaoMostraPalavras = document.querySelector("#botao.palavrachave");
+
+botaoMostraPalavras.addEventListener("clik", mostraPalavrasChave);
+
+function mostraPalavrasChave(){
+    alert("fui clicado!");
+}
