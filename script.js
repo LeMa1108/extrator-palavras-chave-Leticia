e1b970af73e9168ad90d327fed84a9be7ac7f6f3
Leticia Madruga <leticia.madruga@escola.pr.gov.br>
2025-10-28 09:32:32 -0300

    28/10

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave(){
-   const texto = document.querySelector("#entrada-de-texto").value;
-   const campoResultado = document.querySelector("#resultado-palavrachave")
-   const palavras = texto.split(" ");
+function mostraPalavrasChave() {
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = mostraPalavrasChave.join(", ")
+    campoResultado.textContent = palavrasChave.join(", ")
 }
 
-function processaTexto(texto){
+function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
+    return palavras;
+
 }
