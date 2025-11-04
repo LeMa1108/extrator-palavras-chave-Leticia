3ead91f943375b02db326d2de15f044d60c061bf
Leticia Madruga <leticia.madruga@escola.pr.gov.br>
2025-11-04 09:38:14 -0300

    04/11

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ function mostraPalavrasChave() {
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
+    for (Let i in palavras){
+        palavras[i]=palavras[i].toLowerCase();
+    }
+    palavras = tiraPalavrasRuins(palavras);
+
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
